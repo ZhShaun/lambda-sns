d1fb421f6873c1bdbcd87b0b6cd517332edc433d
Your Name <your.email@address>
2025-10-11 10:49:49 +0000

    try commit

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,34 @@
-"""AWS Lambda function that report expired RI to SNS on schedule.
+"""AWS Lambda function that says Hello World to SNS on schedule.
 
 INPUT: EventBridge scheduled event
 OUTPUT: send Hello World to SNS in chatbot format
 """
+
+from __future__ import annotations
+
+import json
+from typing import TYPE_CHECKING
+
+from aws_lambda_powertools.logging import Logger
+
+if TYPE_CHECKING:
+    from aws_lambda_powertools.utilities.data_classes import EventBridgeEvent
+    from aws_lambda_powertools.utilities.typing import LambdaContext
+
+logger = Logger(service="lambda-sns")
+
+
+def lambda_handler(_: EventBridgeEvent, __: LambdaContext) -> json:
+    """Say Hello World."""
+    context = {"message": "Hello World"}
+    logger.info("CloudWatch logs group: %s", context.get("message"))
+
+    # return the calculated area as a JSON string
+    length = 10
+    width = 10
+    return json.dumps(calculate_area(length, width))
+
+
+def calculate_area(length: int, width: int) -> int:
+    """Elementary math."""
+    return length * width
