22549aa56569db836ccea86d1e83d00d48d8b59b
Your Name <your.email@address>
2025-10-12 18:11:00 +0000

    aodjp

--- lambda_function.py
+++ lambda_function.py
@@ -6,6 +6,7 @@ OUTPUT: send Hello World to SNS in chatbot format
 
 from __future__ import annotations
 
+import json
 from typing import TYPE_CHECKING
 
 from aws_lambda_powertools.logging import Logger
@@ -17,9 +18,17 @@ if TYPE_CHECKING:
 logger = Logger(service="lambda-sns")
 
 
-def lambda_handler(_: EventBridgeEvent, __: LambdaContext) -> dict[str, str]:
+def lambda_handler(_: EventBridgeEvent, __: LambdaContext) -> json:
     """Say Hello World."""
     context = {"message": "Hello World"}
     logger.info("CloudWatch logs group: %s", context.get("message"))
 
-    return context
+    # return the calculated area as a JSON string
+    length = 10
+    width = 10
+    return json.dumps(calculate_area(length, width))
+
+
+def calculate_area(length: int, width: int) -> int:
+    """Elementary math."""
+    return length * width
