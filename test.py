926f5352aabe43a6effc036e8d1076639814d3ec
Your Name <your.email@address>
2025-10-27 15:33:10 +0000

    daosihd

---
+++ test.py
@@ -0,0 +1,9 @@
+"""A test module for demonstrating the D100 fix.
+
+It contains a single dummy function for testing purposes.
+"""
+
+
+def test_dummy() -> None:
+    """Dummy function."""
+    return
